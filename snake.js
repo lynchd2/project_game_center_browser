fe0bbb4f6921eaf727c515ae562614f45d692071
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-14 09:16:16 -0400

    Snake finished

--- snake.js
+++ snake.js
@@ -1,88 +1,177 @@
-//sections of a snake
-//Eating food creates new head on snake(link l)
+var model = {
 
-var snakeModel = {
-  // State
-  init: function () {
-    setDirection();
-    setTailCoordinates();
-  },
-  head: [-2,0],
-  tail: [0,0],
-  setTailCoordinates: function() {
-    snakeModel.tail = coordinates;
-  },
-  setDirection: function(coordinates) {
-    snakeModel.head = coordinates;
-  }
-};
-
-var boardModel = {
   init: function() {
-    this.food();
-  },
-  width: 200,
-  height: 200,
-  food: function () {
-    return (Math.random() * 10 + 1);
-  },
-  keys: {
-    38: [-2,0],
-    40: [0,2],
-    39: [2,0],
-    37: [-2,0]
+    this.totalBlanks = 1600;
+    this.rows = 0
+    this.key = 39;
+    this.body = [0,1];
+    this.food = Math.floor(Math.random() * 1600);
+    this.time = 100;
+    this.input = setInterval(view.listenForInput, this.time);
+    this.boundary = setInterval(view.checkBoundaryUpDown, model.time);
+    this.boundary2 = setInterval(view.checkBoundaryLeftRight, model.time);
   }
-};
+
+}
+
+var view = {
+  
+
+  init: function() {
+    this.setBoard();
+    this.listenForInput();
+    this.placeFood();
+    model.input;
+    model.boundary;
+    model.boundary2;
+  },
+
+  resetBoard: function() {
+    controller.init();
+    window.clearTimeout(model.input);
+    window.clearTimeout(model.boundary);
+  },
+
+  placeFood: function() {
+    var food = Math.floor(Math.random() * 1600);
+    $("#" + food).addClass("food");
+    model.food = food;
+    this.makeGameFaster();
+  },
+    
+  listenForInput: function() {
+    $(".total-length").text(model.body.length);
+    view.eatFood();
+    window.onkeyup = function(e) {
+      var newKey = e.keyCode ? e.keyCode : e.which;
+
+      if (model.key == 37 && newKey != 39) {
+          model.key = newKey;
+      }else if (model.key == 38 && newKey != 40) {
+          model.key = newKey;
+      }else if (model.key == 39 && newKey != 37) {
+          model.key = newKey;
+      }else if (model.key == 40 && newKey != 38) {
+          model.key = newKey;
+      }
+    }
+    if (model.key == 37) {
+        view.moveBodyLeft();
+        view.removeBody();
+    }else if (model.key == 38) {
+        view.moveBodyUp();
+        view.removeBody();
+    }else if (model.key == 39) {
+        view.moveBodyRight();
+        view.removeBody();
+    }else if (model.key == 40) {
+        view.moveBodyDown();
+        view.removeBody();
+    }
+  },
+
+  setBoard: function() {
+    $(".body-part").remove();
+    $(".blank-body").remove();
+    while (model.rows < model.totalBlanks) {
+      $(".container").append("<div class='blank-body' id=" + model.rows + "></div>");
+      model.rows ++;
+    }
+  },
+
+  startBody: function() {
+    $("#0").addClass("body-part");
+    $("#1").addClass("body-part");
+  },
+
+  moveBodyRight: function() {
+    for(var i = 0; i < model.body.length; i++) {
+      $("#" + model.body[i]).addClass("body-part")
+    }
+    model.body.push(Number(model.body.slice(-1)) + 1);
+  },
+
+  moveBodyDown: function() {
+    for(var i = 0; i < model.body.length; i++) {
+      $("#" + model.body[i]).addClass("body-part")
+    }
+    model.body.push(Number(model.body.slice(-1)) + 40);
+  },
+
+  moveBodyUp: function() {
+    for(var i = 0; i < model.body.length; i++) {
+      $("#" + model.body[i]).addClass("body-part")
+    }
+    model.body.push(Number(model.body.slice(-1)) - 40);
+  },
+
+  moveBodyLeft: function() {
+    for(var i = 0; i < model.body.length; i++) {
+      $("#" + model.body[i]).addClass("body-part")
+    }
+    model.body.push(Number(model.body.slice(-1)) - 1);
+  },
+
+  removeBody: function() {
+    $("#" + model.body.shift(model.body[0])).removeClass("body-part");
+  },
+
+  checkBoundaryUpDown: function() {
+    if (model.body.slice(-1) > 1600 || model.body.slice(-1) < 0) {
+      view.resetBoard();
+    }
+  },
+
+  checkBoundaryLeftRight: function() {
+    if ((model.body.slice(-2)[1]) % 40 === 0) {
+      if((model.body.slice(-2)[0] + 1) % 40 === 0) {
+        view.resetBoard();
+      }
+    }
+    if ((model.body.slice(-2)[1] + 1) % 40 === 0) {
+      if((model.body.slice(-2)[0]) % 40 === 0) {
+        view.resetBoard();
+      }
+    }
+  },
+
+  eatFood: function() {
+    if(model.body[model.body.length - 1] === model.food){
+      view.growSnake(model.food);
+      view.makeGameFaster();
+      view.removeFood()
+      view.placeFood();
+      
+    }
+  },
+
+  removeFood: function(food) { 
+    $(".food").removeClass("food");
+  },
+
+  growSnake: function(food) {
+    if (model.key == 37) {
+      model.body.unshift(model.body[0] - 1)
+    }else if (model.key == 38) {
+      model.body.unshift(model.body[0] + 40)
+    }else if (model.key == 39) {
+      model.body.unshift(model.body[0] + 1)
+    }else if (model.key == 40) {
+      model.body.unshift(model.body[0] - 40)
+    }
+  },
+
+  makeGameFaster: function() {
+    if(model.body.length > 5) {
+      model.time += 1000;
+    }
+  }
+
+}
 
 var controller = {
   init: function() {
-    cacheDOM();
-    this.listen();
-    view.render(snakeModel,BoardModel);
-  },
-  listen: function() {
-    $snake.on('keypress',function (ev) {
-      snakeModel.setDirection(ev,boardModel);
-    });
-  },
-  cacheDOM: function() {
-    $snake = $('div#snake');
+    model.init();
+    view.init();
   }
-};
-
-var view = {
-  render: function() {
-  }
-};
-
-// What direction the snake is gonna go
-// Where the food will be
-// On keypress event to change snake's direction
-// - snake's head
-// - snake head -> food
-//   -> snake length++
-//   -> delete food
-//   -> add food elsewhere
-// - snake head -> snake
-//   -> game over
-// - snake head -> edge of board
-//   -> game over
-//
-// ## MODELS ##
-// Snake
-//   .head
-//   .length
-//   .direction
-// Board
-//   .width
-//   .height
-//   .food
-//
-//
-// ## CONTROLLERS ##
-// SnakeController
-// -> grabbing state from snake model to view
-// -> updating state from view to snake model(e.g. change direction; growth)
-//
-// ## VIEW ##
-// View
+}
